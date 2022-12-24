4c851333a40f4b68b2c7349140ae9cdf9b533f2e
hyozzi92 <gytjdzja@gmail.com>
2022-12-24 22:38:48 +0900

    modal data props

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="black-bg" v-if="modalStatus == true">
     <div class="white-bg">
-      <h4>상세페이지임</h4>
-      <p>상세페이지 내용임</p>
+      <h4>{{ data[statesNumber].title }}</h4>
+      <p>{{ data[statesNumber].price }}원</p>
+      <img :src="data[statesNumber].image" />
       <button @click="modalStatus = false">닫기</button>
     </div>
     <div class="close-bg" @click="modalStatus = false"></div>
@@ -13,28 +14,32 @@
   </div>
   <div v-for="(a, i) in products" :key="i">
     <img :src="data[i].image" class="room-img" />
-    <h4>{{ data[i].title }}</h4>
+    <h4
+      @click="
+        modalStatus = true;
+        statesNumber = i;
+      "
+    >
+      {{ data[i].title }}
+    </h4>
     <p>{{ data[i].price }}원</p>
   </div>
 </template>
 
 <script>
+console.log(data[0]?.tilte);
 import data from "./data.js";
 
 export default {
   name: "App",
   data() {
     return {
+      statesNumber: 0,
       modalStatus: false,
       biolateNumber: [0, 0, 0, 0],
       price1: [60, 70, 80],
       menu: ["Home", "Shop", "About"],
       products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
-      imgName: [
-        require("./assets/room0.jpg"),
-        require("./assets/room1.jpg"),
-        require("./assets/room2.jpg"),
-      ],
       data: data,
     };
   },
@@ -63,6 +68,8 @@ div {
   padding: 20px;
 }
 .white-bg {
+  display: flex;
+  flex-direction: column;
   width: 100%;
   background: white;
   border-radius: 8px;
