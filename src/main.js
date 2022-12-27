b1d11f6e49d4abf4bfa7287ec2282f207a9da170
heath <jannhyunsoo@gmail.com>
2022-12-27 15:37:25 +0900

    'props'

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+// app.component('AppCard', AppCard)
+app.mount('#app');
+
+
+import 'bootstrap/dist/js/bootstrap.js';
