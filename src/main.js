1facb2276d1eae79c11eb7fd0d7d2317ea5b998e
hyozzi <106464348+hyozzi92@users.noreply.github.com>
2022-12-28 10:03:00 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
+
 import { createApp } from "vue";
 import App from "./App.vue";
 
 createApp(App).mount("#app");
-=======
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { createApp } from 'vue';
-import App from './App.vue';
 
-
-const app = createApp(App)
-// app.component('AppCard', AppCard)
-app.mount('#app');
-
-
-import 'bootstrap/dist/js/bootstrap.js';
->>>>>>> 4bee2cbc26b554eaa131074a05f9d195c211f469
